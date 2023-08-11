9024accc240e673c07eacb4d84a40fc817402a37
Lucas Eduardo Beninca <lebbeninca@gmail.com>
2023-08-11 00:41:33 -0300

    lastajust

--- cypress/e2e/iFrame.cy.js
+++ cypress/e2e/iFrame.cy.js
@@ -5,7 +5,7 @@ describe('Settings page', () => {
 
   it.only('correctly submits a credit card form', () => {
     //run on electron
-    cy.get('.d-sm-flex > .flex-auto').should('have.text','\n This branch is up to date with wlsf82/cyFrame:main.\n  ')
+    cy.get('.d-sm-flex > .flex-auto').should('have.text','\n \n This branch is 4 commits ahead of wlsf82:main.\n \n ')
 
    
     })
